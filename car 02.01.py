90b5cf2f6898b6e3b7d0207cde70ca20a9562c3e
m9139 <m.sli2012@ya.ru>
2024-01-02 18:44:07 +0700

    27.12

---
+++ car 02.01.py
@@ -0,0 +1,14 @@
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for cars in car:
+    print('Я езжу на автомобиле марки', cars)
+
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for cars in car:
+    print('Я езжу на автомобиле марки', cars_count)
+
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count += 10
+for cars_count in range(10):
+    print('Я езжу на автомобиле марки', cars_count)
+
