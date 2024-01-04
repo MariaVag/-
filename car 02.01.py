3de84c1f73908a724962371631de5fcf863b0032
m9139 <m.sli2012@ya.ru>
2024-01-04 15:29:42 +0700

    27.12

--- car 02.01.py
+++ car 02.01.py
@@ -1,14 +1,13 @@
 car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 for cars in car:
-    print('Я езжу на автомобиле марки', cars)
+    print('Я езжу на автомобиле марки', car)
 
 car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
-for cars in car:
+for car in cars:
     print('Я езжу на автомобиле марки', cars_count)
+    cars_count += 10
+    print('машин насчитали', cars_count)
 
-car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-cars_count += 10
-for cars_count in range(10):
-    print('Я езжу на автомобиле марки', cars_count)
+
 
